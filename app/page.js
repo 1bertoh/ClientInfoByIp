63a536af5bbcbebd927a40ff97cbb179c079e9e3
Humberto Junior <humbertoxjunior@hotmail.com>
2024-02-22 17:53:27 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ const Home = () => {
         const fetchData = async () => {
             try {
                 const responseIP = await fetch(
-                    process.env.DOMAIN+"/api/get-ip"
+                    "https://client-info-by-ip.vercel.app" + "/api/get-ip"
                 );
                 const dataIP = await responseIP.json();
                 setIP(dataIP.ip);
