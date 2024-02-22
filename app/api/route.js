9d3146a8fe7957588e3da1f809886d292e2aff74
Humberto Junior <humbertoxjunior@hotmail.com>
2024-02-22 17:05:45 -0300

    Delete route.js

--- app/api/route.js
+++
@@ -1,9 +0,0 @@
-export async function GET(req) {
-    console.log('req', req)
-    const data = {
-        ok: true,
-        ip: req.query?.clientIp ?? "127.0.0.1",
-        message: "Hello from the API",
-    };
-    return new NextResponse(JSON.stringify(data, null, 2));
-}
