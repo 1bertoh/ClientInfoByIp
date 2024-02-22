0a862ee6971836efd9501af126b1ea529a2d8bcb
Humberto Junior <humbertoxjunior@hotmail.com>
2024-02-22 16:43:44 -0300

    Update teste.js

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    res.status(200).json({ info1: req.connection, info2: req.headers });
+    res.status(200).json({ info1: req.connection });
 }
