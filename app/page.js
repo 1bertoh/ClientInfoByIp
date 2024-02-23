c246ccd4baba00bbfcbbf60a5c4ebedf3ed3821d
Humberto Junior <humbertoxjunior@hotmail.com>
2024-02-22 23:25:30 -0300

    turning the page on client and the server returns the complete data

--- app/page.js
+++ app/page.js
@@ -1,48 +1,81 @@
 'use client'
 
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import Field from "./_components/Field";
-import MountainColored from "@/icons/mountain-colored";
-import Mountain from "@/icons/mountain";
-import WaterDropColored from "@/icons/water-drop-colored";
+import Image from "next/image";
+import Field from "@/app/_components/Field";
 import DropWater from "@/icons/water-drop";
+import Mountain from "@/icons/mountain";
+import Sun from "@/icons/sun";
+import getIP from "@/api/getIP";
+import getIsOnWaterAPI from "@/api/getIsOnWaterAPI";
+import getWeatherAPI from "@/api/getWeatherAPI";
+import WaterDropColored from "@/icons/water-drop-colored";
+import MountainColored from "@/icons/mountain-colored";
+import Tooltip from "@/app/_components/Tooltip";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    const router = useRouter();
+    const [userInfo, setUserInfo] = useState(null)
+
     const local = [
         {
-            icon: () => <DropWater className="h-auto w-7" />,
+            icon: (isOnWater) =>
+                isOnWater ? (
+                    <WaterDropColored className="h-auto w-7" />
+                ) : (
+                    <DropWater className="h-auto w-7" />
+                ),
             text: "Sim",
             className: (isOnWater) =>
                 isOnWater ? "on-local-water" : "input-bg-color",
         },
         {
-            icon: () => <Mountain className="h-auto w-7" />,
+            icon: (isOnWater) =>
+                !isOnWater ? (
+                    <MountainColored className="h-auto w-7" />
+                ) : (
+                    <Mountain className="h-auto w-7" />
+                ),
             text: "Não",
             className: (isOnWater) =>
                 !isOnWater ? "on-local-ground" : "input-bg-color",
         },
     ];
 
-    function getIP() {
+    useEffect(() => {
         axios.get("https://client-info-by-ip.vercel.app/api/get-ip")
-        .then(e => {
-            const ip = e.data.ip
-              router.push("/ip/"+ip);
-            }).then(e => {
-                
-        })
-    }
+        .then(e => setUserInfo(e.data))
+    },[])
 
     return (
         <main className="container min-h-screen pt-4 sm:text-lg text-base">
+        {
+            userInfo &&
+            <UserForm useInfo={userInfo} />
+
+        }
+        </main>
+    );
+}
+
+/**
+ * 
+ * @param {{
+ * ip:String, userInfo:{latitute:Number,longitute:Number,regionName:String, city:String},
+ * isOnWater:{isOnWater:Boolean},weather:{code:String, temp:Number, condition_slug:String, icon:SVG}
+ * }} userData
+ * @returns 
+ */
+const UserForm = (userData) => {
+    const {ip, isOnWater, userInfo, weather} = userData
+    return (
+        <>
             <Field
                 isDisabled={true}
                 label={"IP do computador"}
                 prefix={"IP"}
                 id="ip-comp"
-                value={"000.000.000.000"}
+                value={ip}
             />
 
             <div className="mt-10">
@@ -50,10 +83,14 @@ export default function Home() {
                     isDisabled={true}
                     label="Dados do usuário"
                     prefix={"Cidade"}
-                    value={"Sua cidade"}
+                    value={userInfo.city}
                 />
                 <br />
-                <Field isDisabled={true} prefix={"UF"} value={"Seu País"} />
+                <Field
+                    isDisabled={true}
+                    prefix={"UF"}
+                    value={userInfo.regionName}
+                />
             </div>
             <div className="sm:flex gap-3 mt-20 items-center">
                 <span className="sm:block font-bold">Está na água?</span>
@@ -62,23 +99,45 @@ export default function Home() {
                         return (
                             <div
                                 key={l.text}
-                                className={`flex p-3 rounded-lg gap-3 w-min input-bg-color`}
+                                className={`flex p-3 rounded-lg gap-3 w-min ${l.className(
+                                    isOnWater.isOnWater
+                                )}`}
                             >
                                 <span>{l.text}</span>
-                                {l.icon()}
+                                {l.icon(isOnWater.isOnWater)}
                             </div>
                         );
                     })}
                 </div>
             </div>
             <div className="mt-10">
-                <div
-                    className="rounded-xl input-bg-color hover:bg-teal-400 p-2 text-center font-semibold cursor-pointer "
-                    onClick={getIP}
-                >
-                    Check seu IP
+                <span className="font-bold sm:block">Está muito calor?</span>
+                <div className="input-bg-color overflow-hidden mt-2 rounded-xl">
+                    <div
+                        className="inline-block "
+                        style={{
+                            marginLeft: "50%",
+                            transform: "translate(-50%)",
+                        }}
+                    >
+                        <Tooltip
+                            msg={
+                                weather.condition_slug === "clear_day"
+                                    ? "Muito"
+                                    : "Pouco"
+                            }
+                            className="mx-auto"
+                        >
+                            <Image
+                                width={220}
+                                height={220}
+                                alt="weather"
+                                src={`https://assets.hgbrasil.com/weather/icons/conditions/${weather.condition_slug}.svg`}
+                            />
+                        </Tooltip>
+                    </div>
                 </div>
             </div>
-        </main>
-    );
+        </>
+    )
 }
