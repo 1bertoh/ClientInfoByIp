f7f64bf5da03c424c030c5128fbe35012f1c0ed0
Humberto Junior <humbertoxjunior@hotmail.com>
2024-02-22 23:02:51 -0300

    the api is going to return all the data

--- pages/api/get-ip.js
+++ pages/api/get-ip.js
@@ -1,7 +1,19 @@
+import getIP from "@/api/getIP";
+import getWeatherAPI from "@/api/getWeatherAPI";
+
 export default async function handler(req, res) {
     const ip =
         req.headers["x-real-ip"] ||
         req.headers["x-forwarded-for"] ||
         req.connection.remoteAddress;
-    res.status(200).json({ ip: ip });
+
+    const userInfo = await getIP(ip);
+    const lat = userInfo.latitude;
+    const lon = userInfo.longitude;
+    // const isOnWater = await getIsOnWaterAPI(lat, lon) //SÓ FUNCIONA LOCALMENTE
+    const isOnWater = { isOnWater: false };
+
+    const weather = await getWeatherAPI(lat, lon);
+
+    res.status(200).json({ ip, userInfo, isOnWater, weather });
 }
