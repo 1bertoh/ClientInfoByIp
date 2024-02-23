e320ec4b86ad8afedeaebc5ff1ade528bcfa7b3f
Humberto Junior <humbertoxjunior@hotmail.com>
2024-02-23 00:26:18 -0300

    fixing some visual issue, too light letters

--- app/_components/Field.js
+++ app/_components/Field.js
@@ -14,14 +14,14 @@ const Field = ({ label, prefix, value, isDisabled, id }) => {
                 )
             }
             <div className="relative w-" style={{width: 'initial'}}>
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                <div className="absolute inset-y-0 start-0 font-medium flex items-center ps-3.5 pointer-events-none">
                     {prefix}
                 </div>
                 <input
                     type="text"
                     disabled={isDisabled}
                     id={id}
-                    className="rounded-lg cursor-not-allowed block w-full ps-20 p-3  "
+                    className="rounded-lg cursor-not-allowed font-medium block w-full ps-20 p-3  "
                     value={value}
                 />
             </div>
